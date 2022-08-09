9ce6c3de2f672bba092cde8b31e5d68895b49f20
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-08-08 21:46:22 -0300

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './core/imports.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './views/Home.view';
-import Contact from './views/Contact.view';
+import Home from './app/views/Home.view';
+import Contact from './app/views/Contact.view';
+import NotFound404 from './app/views/NotFound404.view';
+import NavBar from './app/components/NavBar';
+import GlobalStyles from './core/globalStyles';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <NavBar />
       <Switch>
-        <Route path={'/home'}>
-          <Home />
-        </Route>
-        <Route path={'/contato'}>
-          <Contact />
-        </Route>
-        <Route>
-          {/* <NotFound /> */}
-        </Route>
+        <Route path={'/'} exact component={Home} />
+        <Route path={'/contato'} exact component={Contact} />
+        <Route component={NotFound404} />
       </Switch>
     </BrowserRouter>
+    <GlobalStyles />
   </React.StrictMode>
 );
 
