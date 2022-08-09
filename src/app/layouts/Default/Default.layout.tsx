dceb80efee1c56476077a40d7d0a4cbc748cc7ac
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-08-09 20:18:13 -0300

    commit

--- src/app/layouts/Default/Default.layout.tsx
+++ src/app/layouts/Default/Default.layout.tsx
@@ -1,4 +1,6 @@
+import Logo from "../../components/Logo";
 import NavBar from "../../components/NavBar";
+import SessionController from "../../components/SessionController";
 import * as DL from "./Default.layout.styles";
 
 interface DefaultLayoutProps {
@@ -9,7 +11,7 @@ function DefaultLayout(props: DefaultLayoutProps) {
   return (
     <DL.Wrapper>
       <DL.Header>
-        Header
+        <Logo />
       </DL.Header>
       <DL.Main>
         <DL.Navigation>
@@ -19,7 +21,10 @@ function DefaultLayout(props: DefaultLayoutProps) {
           {props.children}
         </DL.FeaturedContent>
         <DL.Aside>
-          sidebar right
+          <SessionController
+            name="Eduardo Moreira"
+            description="Editor há mais de 10 anos"
+          />
         </DL.Aside>
       </DL.Main>
     </DL.Wrapper>
