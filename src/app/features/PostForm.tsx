33dd3375252bf188f9c622073520d7dae0315f88
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-08-15 10:55:58 -0300

    commit

--- src/app/features/PostForm.tsx
+++ src/app/features/PostForm.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { Tag } from "react-tag-input"
 import styled from "styled-components"
 import info from "../../core/utils/info"
+import PostService from "../../sdk/services/Post.service"
 import Button from "../components/Button/Button"
 import ImageUpload from "../components/ImageUpload"
 import Input from "../components/Input/Input"
@@ -11,28 +12,41 @@ import WordPriceCounter from "../components/WordPriceCounter"
 
 export default function PostForm() {
   const [tags, setTags] = useState<Tag[]>([])
+  const [body, setBody] = useState('')
+  const [title, setTitle] = useState('')
 
-  function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+  async function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
+    const newPost = {
+      body,
+      title,
+      tags: tags.map(tag => tag.text),
+      imageUrl: ''
+    }
+
+    const insertedPost = await PostService.insertNewPost(newPost);
+
     info({
-      title: "Post salvo com sucesso",
-      description: "Você acabou de salvar o post."
+      title: title,
+      description: `Você publicou um novo post com o id ${insertedPost.id}`,
     })
   }
 
   return <PostFormWrapper onSubmit={handleFormSubmit}>
     <Input
       label="título"
+      value={title}
+      onChange={e => setTitle(e.target.value)}
       placeholder="e.g.: Como fiquei rico aprendendo React"
     />
     <ImageUpload label="Thumbnail do post" />
-    <MarkdownEditor />
-    {/* <TagInput
+    <MarkdownEditor onChange={setBody} />
+    <TagInput
       tags={tags}
       onAdd={tag => setTags([...tags, tag])}
       onDelete={index => setTags(tags.filter((_, i) => i !== index))}
       placeholder="Insira as tags deste post"
-    /> */}
+    />
     <PostFormSubmitWrapper>
       <WordPriceCounter pricePerWord={0.25} wordsCount={40} />
       <Button variant="primary" label="Salvar post" type="submit" />
