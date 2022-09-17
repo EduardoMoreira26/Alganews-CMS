eaff3a749a2ee03ec2ccd4d6a5f1811d2853021e
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-09-17 19:34:30 -0300

    commi

--- src/app/components/ImageUpload/ImageUpload.tsx
+++ src/app/components/ImageUpload/ImageUpload.tsx
@@ -1,13 +1,13 @@
 import { mdiUpload } from '@mdi/js'
 import Icon from '@mdi/react'
 import { ChangeEvent, useState } from 'react'
-import { uuid } from 'uuidv4'
 import FileService from '../../../sdk/services/File.service'
 import Button from '../Button/Button'
 import * as IU from './ImageUpload.styles'
 
 export interface ImageUploadProps {
   label: string
+  onImageUpload: (imageUrl: string) => any
 }
 
 function ImageUpload (props: ImageUploadProps) {
@@ -19,17 +19,10 @@ function ImageUpload (props: ImageUploadProps) {
     if (file) {
       const reader = new FileReader()
 
-      reader.addEventListener('load', async  e => {
+      reader.addEventListener('load', async e => {
         setFilePreview(String(e.target?.result));
-        
-        const [extension] = file.name.split('.').slice(-1);
-
-        const sinegdUrl = await FileService.getSignedUrl({
-          fileName: `${uuid()}.${extension}`,
-          contentLength: file.size,
-        })
-
-        await FileService.uploadFileToSignedUrl(sinegdUrl, file);
+        const imageUrl = await FileService.upload(file)
+        props.onImageUpload(imageUrl)
       })
 
       reader.readAsDataURL(file)
